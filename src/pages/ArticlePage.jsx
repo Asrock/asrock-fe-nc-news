ab6da15408bc66e20c586f8ab81c1e7dc52a5091
Asrock <soad@jerez.es>
2023-12-14 12:20:19 +0000

    fix indentation

--- src/pages/ArticlePage.jsx
+++ src/pages/ArticlePage.jsx
@@ -24,9 +24,9 @@ const ArticlePage = () => {
                 setComments(comments);
                 setApiStatus({ status: "completed" });
             })
-			.catch(({ response, message }) => {
-				setApiStatus({ status: "error", message: response.data.message ?? message });
-			});
+            .catch(({ response, message }) => {
+                setApiStatus({ status: "error", message: response.data.message ?? message });
+            });
     }, [])
 
     return ApiManager(apiStatus,
