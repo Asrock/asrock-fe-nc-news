04dc397e5b2b00242803a134b794a077f8e05e67
Asrock <soad@jerez.es>
2023-12-14 14:22:22 +0000

    add - voting to article and comment - CSS Styles

--- src/components/CommentCard.jsx
+++ src/components/CommentCard.jsx
@@ -1,10 +1,41 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
+import { useState } from "react";
 import { relativeTimeFormatter } from "../../utils/formatter-utils";
+import apiRequest from "../../utils/api-utils";
 
-const CommentCard = ({ comment: { body, created_at, author } }) => {
+const CommentCard = ({ comment: { body, created_at, author, votes: currVotes, comment_id, article_id } }) => {
+    const [votes, setVotes] = useState(currVotes);
+
+    const voteHandler = (num) => ({ target }) => {
+        const oppositeBtn = [...target.parentElement.children].find(field => field.nodeName === "BUTTON" && field !== target);
+
+        [target, oppositeBtn].forEach((btn) => btn.classList.remove("vote-error"));
+
+        const relativeUrlPath = comment_id == null ? `articles/${article_id}` : `comments/${comment_id}`;
+        apiRequest("patch", relativeUrlPath, { inc_votes: num })
+            .then(({ data }) => {
+                setVotes((data.article ?? data.comment)["votes"]);
+
+                if(oppositeBtn.classList.contains("vote-success"))
+                    [target, oppositeBtn].forEach((btn) => btn.classList.remove("vote-success"));
+                else
+                    target.classList.add("vote-success");
+            })
+            .catch(() => {
+                target.classList.add("vote-error");
+            });
+    };
+
     return (
         <div className="comment-card">
-            <p>{body}</p>
+            <div className="comment">
+                <div className="voting">
+                    <button className="round" onClick={voteHandler(1)}>&#x25B3;</button>
+                    <p>{votes}</p>
+                    <button className="round" onClick={voteHandler(-1)}>&#x25BD;</button>
+                </div>
+                <p>{body}</p>
+            </div>
             <div className="inline">
                 <h5>Created by</h5>
                 <Link to={`/users/${author}`}><h5>{author}</h5></Link>
