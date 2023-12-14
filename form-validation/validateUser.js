9e0707219aa6415a46c1a71e6eb3c08c33a562e4
Asrock <soad@jerez.es>
2023-12-14 09:50:32 +0000

    refactor: remove unused code

--- form-validation/validateUser.js
+++ form-validation/validateUser.js
@@ -9,13 +9,6 @@ const validateUser = ({target}, forceValidation) => {
       if (/^[a-z0-9_-]{3,15}$/i.test(target.value))
         return target.classList.add("valid-input");
       break;
-    case "avatar_url":
-      if (/^$|(?:^(http(s?):)([\/.\w\s\-+_])*\.(?:jpg|gif|png)$)/.test(target.value))
-        return target.classList.add("valid-input");
-      break;
-    case "name":
-      if (/^[a-z-\s]{2,15}$/i.test(target.value))
-        return target.classList.add("valid-input");
     default: return;
   }
   return target.classList.add("invalid-input");
