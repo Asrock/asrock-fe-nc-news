e7cac4c80ad367dcdeb5610e84558deceba2f4b7
Asrock <soad@jerez.es>
2023-12-14 10:17:20 +0000

    fix login add error message

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,10 +1,11 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { UserContext } from "../contexts/UserContext";
 import { useNavigate } from "react-router-dom";
 import validateUser from "../../form-validation/validateUser";
 
 const LoginPage = () => {
     const { loginAsync } = useContext(UserContext);
+    const [errorMessage, setErrorMessage] = useState();
     const navigate = useNavigate();
 
     const onSubmit = (event) => {
@@ -12,16 +13,17 @@ const LoginPage = () => {
         const inputs = event.target.children;
 
         const isValid = [...inputs].reduce((result, field) => {
+            if (field.nodeName !== "INPUT") return result;
             validateUser({ target: field }, true);
             return result && field.classList.contains("valid-input");
         }, true);
-        if (!isValid) return;
+        if (!isValid) return setErrorMessage("Invalid username");
 
-        loginAsync(inputs[0].value)
+
+        loginAsync(inputs[1].value)
             .then(() => navigate("/"))
-            .catch(() => {
-                inputs[0].classList.remove("valid-input");
-                inputs[0].classList.add("invalid-input");
+            .catch(({response, message}) => {
+                setErrorMessage(response.data.message ?? message)
             });
     }
 
@@ -33,6 +35,7 @@ const LoginPage = () => {
                 <input id="username" type="text" onKeyUp={validateUser} />
                 <button>Log in</button>
             </form>
+            <p>{errorMessage}</p>
         </div>
     )
 };
